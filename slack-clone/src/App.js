b38492844d59abb2741c574f4a85e7fbeb394c80
DadeCountyCoder <dadecountycoder@gmail.com>
2020-11-16 15:52:45 -0500

    make header

---
+++ slack-clone/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react' ;
+import './App.css';
+
+function App() {
+  return (
+    // BEM naming convention
+    <div className="app">
+      <h1> Hello Coder! Let's build a slack clone! </h1>
+     {/* Header */}
+     {/* Sidebar */}
+     {/* React-Router -> Chat screen */}
+
+    </div>
+  );
+}
+
+export default App;
+
+
